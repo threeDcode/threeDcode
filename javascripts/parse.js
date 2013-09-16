30b13de7e3692a3caf9565844836542f7ca3c821
Chuphay <chuphay@gmail.com>
2013-09-16 17:07:08 +0800

    fixed a bug in the logout

--- javascripts/parse.js
+++ javascripts/parse.js
@@ -43,7 +43,7 @@ function logout(){
         Parse.User.logOut();
         var oldCode = document.getElementById("code");
         myCodeMirror.setValue(oldCode.value)
-       return getStatus();
+       return updateStatus();
   
 }           
 
